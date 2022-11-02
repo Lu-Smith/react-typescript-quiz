fce39c6b6e9bdc7fd16b3dc68b19c0a08d6b7483
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-11-02 19:56:52 +0000

    Updated QuestionCard.tsx

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 
-const QuestionCard = () => {
+type Props = {
+    question: string;
+    answer: string[];
+    callback: any;
+    userAnswer: string;
+    questionNumber: number;
+    totalQuestions: number;
+}
+
+const QuestionCard: React.FC<Props> = ({question, answer, callback, userAnswer, questionNumber, totalQuestions}) => {
   return (
-    <div>QuestionCard</div>
+    <div>
+        <p className='number'>
+            Question: {questionNumber} / {totalQuestions}
+        </p>
+        
+    </div>
   )
 }
 
