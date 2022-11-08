6a87e2bdf5a493611e22ff3998b561bd6d6b304d
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-11-08 19:30:06 +0000

    Updated Main.tsx and API.ts

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,12 +1,16 @@
 import React, {useState} from 'react';
 import QuestionCard from './QuestionCard';
 
-
+const Total_Questions = 10;
 
 const Main = () => {
 
     const [loading, setLoading] = useState(false);
     const [questions, setQuestions] = useState([]);
+    const [number, setNumber] = useState(0);
+    const [userAnswers, setUserAnswers] = useState([]);
+    const [score, setScore] = useState(0);
+    const [gameOver, setGameOver] = useState(true);
 
     const startTrivia = async () => {
 
@@ -28,7 +32,13 @@ const Main = () => {
      </button>
      <p className='score'>Score:</p>
      <p className='loading'>Loading questions...</p>
-     <QuestionCard />
+     {/* <QuestionCard 
+     questionNumber={number + 1}
+     totalQuestions={Total_Questions} 
+     question={questions[number].question}
+     answers={questions[number].answers}
+     userAnswer={userAnswers ? userAnswers[number] : undefined}
+     callback={checkAnswer}/> */}
      <button className='next' onClick={nextQuestoin}>Next question</button>
     </div>
   )
