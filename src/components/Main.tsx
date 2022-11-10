f50526d9284815293f405e3396276924cff6e6c9
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-11-10 19:53:22 +0000

    Updated Main.tsx
    Add value to the button in QuestionCard.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -4,7 +4,7 @@ import QuestionCard from './QuestionCard';
 
 type AnswerObject = {
   question: string;
-  answers: string;
+  answer: string;
   correct: boolean;
   correctAnswer: string;
 };
@@ -35,7 +35,19 @@ const Main = () => {
     console.log(questions);
   
     const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
-  
+      if (!gameOver) {
+        const answer = e.currentTarget.value;
+        const correct = questions[number].correct_answer === answer;
+        if (correct) setScore(prev => prev + 1);
+        const answerObject = {
+          question: questions[number].question,
+          answer,
+          correct,
+          correctAnswer: questions[number].correct_answer,
+        };
+        setUserAnswers(prev => [...prev, answerObject])
+
+      }
     };
   
     const nextQuestion = () => {
@@ -67,7 +79,6 @@ const Main = () => {
      number !== Total_Questions -1 ? (
       <button className='next' onClick={nextQuestion}>Next question</button>
     ) : null}
-     
     </div>
   )
 }
