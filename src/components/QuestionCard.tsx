1e014ee3b0927e6020df3e927b4178ca4937a74b
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-11-10 20:03:45 +0000

    Updated QuestionCard.tsx

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import {AnswerObject} from './Main';
 
 type Props = {
     question: string;
     answers: string[];
     callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
-    userAnswer: any;
+    userAnswer: AnswerObject | undefined;
     questionNumber: number;
     totalQuestions: number;
 }
@@ -19,7 +20,7 @@ const QuestionCard: React.FC<Props> = ({question, answers, callback, userAnswer,
         <div>
              {answers.map(answer => (
                 <div key={answer}>
-                   <button disabled={userAnswer} value ={answer} onClick={callback}>
+                   <button disabled={userAnswer ? true : false} value ={answer} onClick={callback}>
                      <span dangerouslySetInnerHTML={{__html: answer}} /> 
                    </button> 
                 </div>
