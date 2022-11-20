eeba9e5285866d5ed6798b2bf68c9c2a6b2f383c
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-11-20 17:19:24 +0000

    Created Footer.styles.ts

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { Container } from './styles/Footer.styles'
 
 const Footer = () => {
   return (
-    <div className='Footer'>Footer</div>
+    <Container>Coded by <a href='https://www.lunasmithart.com/' target="_blank" rel="noreferrer">Luna Smith</a>, open-source on <a href='https://github.com/Lu-Smith/react-typescript-quiz' target="_blank" rel="noreferrer">GitHub</a>.</Container>
   )
 }
 
---
+++ src/components/styles/Footer.styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+ font-style: italic;
+ color: #e0ebeb; 
+ background-color: #a7bcb9; 
+ padding: 15px 20px;
+ font-size: 20px;
+
+ a {
+    text-decoration: none;
+    color: #e0ebeb; 
+ }
+`
